be9f817cfad3511d29092f21d87572cb64a4e323
AreusDZ <samir-mekhloufi@hotmail.fr>
2020-11-03 18:22:43 +0100

    Petites améliorations du visuel

--- TP_SQL/Classes/inscription.php
+++ TP_SQL/Classes/inscription.php
@@ -1,7 +1,7 @@
 <html>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
-    <div class="container-fluid" style="width : 50%">
+    <div class="container" style="width : 50%; padding:5%; background-color: indigo; margin-top:5%;">
             <form action="traitement.php" method="POST">
             <div class="form-group">
                 <label for="email">Email</label>
